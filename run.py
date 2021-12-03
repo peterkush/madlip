1c6063b5062d6fff5fefa93726ae1437b02ad313
peterkush <pkungu004@gmail.com>
2021-12-03 12:29:24 +0300

    create madlip

---
+++ run.py
@@ -0,0 +1,9 @@
+adj = input('Adjective..')
+verb1 =input('verb..')
+verb2 =input('verb..')
+famous_person=input('famous person')
+
+madlib= 'computer programming is so {adj} ! It makes me soexcited all the time because\I live to {verb1}.Stay hydrated and {verb2} like you are {famous_person}!'
+
+
+print(madlib)
